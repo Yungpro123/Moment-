4a6100fddabae08426e05d65334e734cccaba9ce
Daniel hood <ciprusprime82@gmail.com>
2023-09-23 10:20:37 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function loader(){
+  var e = document.getElementById("min")
+  e.style.display = "none"
+}
+
+setTimeout(loader,1000)
+
+
+
+
+menubtn.onclick = () =>{
+var moo = document.getElementById("mnu").style.left = "0"
+mmo.style.display = "initial"
+}
+
+function mop(){
+  
+var moo = document.getElementById("mnu").style.left = "-250px"
+mmo.style.display = "none"
+  
+}
+
+ function name() {
+   
+ var mipl = document.createElement("div")
+mipl.id = "hlpp"
+mipl.style.display = "flex"
+var tt = ' <div id="icn" onclick="name()" class="mio" style="bottom: 0;margin:0 auto;"><img src="cancel.svg"  alt="cancel.svg" id="rotate:hover"></div>'
+document.body.appendChild(mipl)
+mipl.innerHTML += tt
+ }
+ 
+ 
